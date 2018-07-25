8d4e8abd902f328e4166148610e87805fafecd55
PrzemyslawKlys <pklys@evotec.pl>
2018-07-25 14:57:28 +0200

    Merged some code from Lewis Roberts

--- DomainControllerInfrastructure.tests.ps1
+++ DomainControllerInfrastructure.tests.ps1
@@ -10,6 +10,7 @@ Describe -Name 'Domain Controller Infrastructure Test' -Tags 'Infrastructure,Dom
     It -Name 'Active Directory is available' {
         $DCS | Should -Not -BeNullOrEmpty
     }
+    if ($DCS -eq $null) { return }
     foreach ($DC01 in $DCS) {
         Context -Name "$DC01 Availability" {
 
@@ -50,5 +51,37 @@ Describe -Name 'Domain Controller Infrastructure Test' -Tags 'Infrastructure,Dom
             }
         }
         #room for future tests if needed
+    }
+    Context 'Replication Link Status' {
+
+        $results = repadmin /showrepl * /csv | ConvertFrom-Csv # Get the results of all replications between all DCs
+
+        $groups = $results | Group-Object -Property 'Source DSA' # Group the results by the source DC
+
+        foreach ($sourcedsa in $groups) {
+            # Create a context for each source DC
+
+            Context "Source DSA = $($sourcedsa.Name)" {
+
+                $targets = $sourcedsa.Group # Assign the value of the groupings to another var since .Group doesn't implement IComparable
+
+                $targetdsa = $targets | Group-Object -Property 'Destination DSA' # Now group within this source DC by the destination DC (pulling naming contexts per source and destination together)
+
+                foreach ($target in $targetdsa ) {
+                    # Create a context for each destination DSA
+
+                    Context "Target DSA = $($target.Name)" {
+
+                        foreach ($entry in $target.Group) {
+                            # List out the results and check each naming context for failures
+
+                            It "$($entry.'Naming Context') - should have zero replication failures" {
+                                $entry.'Number of failures' | Should Be 0
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
