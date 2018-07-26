1ee4cebda91bdaf6ee70a1721c32a974caa498a0
PrzemyslawKlys <pklys@evotec.pl>
2018-07-26 18:22:51 +0200

    Updated to support all domains of a forest

--- DomainControllerInfrastructure.tests.ps1
+++ DomainControllerInfrastructure.tests.ps1
@@ -1,82 +1,94 @@
 # These can possibly be stripped out if a control script is used.
 Import-Module Pester
 
-Describe -Name 'Domain Controller Infrastructure Test' -Tags 'Infrastructure,Domain Controllers' {
+Describe -Name 'Domain Controller Infrastructure Test' {
     try {
-        $DCS = (Get-ADDomainController -Filter * -ErrorAction Stop | Select HostName).HostName
+        $AllDomains = (get-adforest -ErrorAction Stop).Domains
     } catch {
-        $DCS = $null
+        $AllDomains = $null
     }
-    It -Name 'Active Directory is available' {
-        $DCS | Should -Not -BeNullOrEmpty
-    }
-    if ($DCS -eq $null) { return }
-    foreach ($DC01 in $DCS) {
-        Context -Name "$DC01 Availability" {
 
-            It -Name "$DC01 Responds to Ping" {
-                $Ping = Test-NetConnection -ComputerName $DC01
-                $Ping.PingSucceeded | Should -Be $true
-            }
-            It -Name "$DC01 Responds on Port 53" {
-                $Port = Test-NetConnection -ComputerName $DC01 -Port 53
-                $Port.TcpTestSucceeded | Should -Be $true
-            }
-            It -Name "$DC01 DNS Service is Running" {
-                $DNSsvc = Get-Service -ComputerName $DC01 -DisplayName 'DNS Server' -ErrorAction Stop
-                $DNSsvc.Status | Should -BeExactly 'Running'
-            }
-            It -Name "$DC01 ADDS Service is Running" {
-                $NTDSsvc = Get-Service -ComputerName $DC01 -DisplayName 'Active Directory Domain Services' -ErrorAction Stop
-                $NTDSsvc.Status | Should -BeExactly 'Running'
-            }
-            It -Name "$DC01 ADWS Service is Running" {
-                $ADWSsvc = Get-Service -ComputerName $DC01 -DisplayName 'Active Directory Web Services' -ErrorAction Stop
-                $ADWSsvc.Status | Should -BeExactly 'Running'
-            }
-            It -Name "$DC01 KDC Service is Running" {
-                $KDCsvc = Get-Service -ComputerName $DC01 -DisplayName 'Kerberos Key Distribution Center' -ErrorAction Stop
-                $KDCsvc.Status | Should -BeExactly 'Running'
-            }
-            It -Name "$DC01 Netlogon Service is Running" {
-                $Netlogonsvc = Get-Service -ComputerName $DC01 -DisplayName 'Netlogon' -ErrorAction Stop
-                $Netlogonsvc.Status | Should -BeExactly 'Running'
-            }
+    It -Name 'Active Directory Forest is available' {
+        $AllDomains | Should -Not -BeNullOrEmpty
+    }
+    if ($AllDomains -eq $null) { return }
+    foreach ($Domain in $AllDomains) {
+        try {
+            $DCS = (Get-ADDomainController -Server $Domain -Filter * -ErrorAction Stop | Select HostName).HostName
+        } catch {
+            $DCS = $null
         }
-        Context -Name "Replication Status" {
-            It -Name "$DC01 Last Replication Result is 0 (Success)" {
-                $RepResult = Get-ADReplicationPartnerMetaData -Target "$DC01" -PartnerType Both -Partition *
-                # using $null because success is 0, and that is considered a null value
-                $RepResult.LastReplicationResult | Should -BeIn $null, 0
-            }
+        It -Name 'Active Directory Domain is available' {
+            $DCS | Should -Not -BeNullOrEmpty
         }
-        #room for future tests if needed
-    }
-    Context 'Replication Link Status' {
+        if ($DCS -eq $null) { return }
+        foreach ($DC01 in $DCS) {
+            Context -Name "$DC01 Availability" {
 
-        $results = repadmin /showrepl * /csv | ConvertFrom-Csv # Get the results of all replications between all DCs
+                It -Name "$DC01 Responds to Ping" {
+                    $Ping = Test-NetConnection -ComputerName $DC01
+                    $Ping.PingSucceeded | Should -Be $true
+                }
+                It -Name "$DC01 Responds on Port 53" {
+                    $Port = Test-NetConnection -ComputerName $DC01 -Port 53
+                    $Port.TcpTestSucceeded | Should -Be $true
+                }
+                It -Name "$DC01 DNS Service is Running" {
+                    $DNSsvc = Get-Service -ComputerName $DC01 -DisplayName 'DNS Server' -ErrorAction Stop
+                    $DNSsvc.Status | Should -BeExactly 'Running'
+                }
+                It -Name "$DC01 ADDS Service is Running" {
+                    $NTDSsvc = Get-Service -ComputerName $DC01 -DisplayName 'Active Directory Domain Services' -ErrorAction Stop
+                    $NTDSsvc.Status | Should -BeExactly 'Running'
+                }
+                It -Name "$DC01 ADWS Service is Running" {
+                    $ADWSsvc = Get-Service -ComputerName $DC01 -DisplayName 'Active Directory Web Services' -ErrorAction Stop
+                    $ADWSsvc.Status | Should -BeExactly 'Running'
+                }
+                It -Name "$DC01 KDC Service is Running" {
+                    $KDCsvc = Get-Service -ComputerName $DC01 -DisplayName 'Kerberos Key Distribution Center' -ErrorAction Stop
+                    $KDCsvc.Status | Should -BeExactly 'Running'
+                }
+                It -Name "$DC01 Netlogon Service is Running" {
+                    $Netlogonsvc = Get-Service -ComputerName $DC01 -DisplayName 'Netlogon' -ErrorAction Stop
+                    $Netlogonsvc.Status | Should -BeExactly 'Running'
+                }
+            }
+            Context -Name "Replication Status" {
+                It -Name "$DC01 Last Replication Result is 0 (Success)" {
+                    $RepResult = Get-ADReplicationPartnerMetaData -Target "$DC01" -PartnerType Both -Partition *
+                    # using $null because success is 0, and that is considered a null value
+                    $RepResult.LastReplicationResult | Should -BeIn $null, 0
+                }
+            }
+            #room for future tests if needed
+        }
+        Context 'Replication Link Status' {
 
-        $groups = $results | Group-Object -Property 'Source DSA' # Group the results by the source DC
+            $results = repadmin /showrepl * /csv | ConvertFrom-Csv # Get the results of all replications between all DCs
 
-        foreach ($sourcedsa in $groups) {
-            # Create a context for each source DC
+            $groups = $results | Group-Object -Property 'Source DSA' # Group the results by the source DC
 
-            Context "Source DSA = $($sourcedsa.Name)" {
+            foreach ($sourcedsa in $groups) {
+                # Create a context for each source DC
 
-                $targets = $sourcedsa.Group # Assign the value of the groupings to another var since .Group doesn't implement IComparable
+                Context "Source DSA = $($sourcedsa.Name)" {
 
-                $targetdsa = $targets | Group-Object -Property 'Destination DSA' # Now group within this source DC by the destination DC (pulling naming contexts per source and destination together)
+                    $targets = $sourcedsa.Group # Assign the value of the groupings to another var since .Group doesn't implement IComparable
 
-                foreach ($target in $targetdsa ) {
-                    # Create a context for each destination DSA
+                    $targetdsa = $targets | Group-Object -Property 'Destination DSA' # Now group within this source DC by the destination DC (pulling naming contexts per source and destination together)
 
-                    Context "Target DSA = $($target.Name)" {
+                    foreach ($target in $targetdsa ) {
+                        # Create a context for each destination DSA
 
-                        foreach ($entry in $target.Group) {
-                            # List out the results and check each naming context for failures
+                        Context "Target DSA = $($target.Name)" {
 
-                            It "$($entry.'Naming Context') - should have zero replication failures" {
-                                $entry.'Number of failures' | Should Be 0
+                            foreach ($entry in $target.Group) {
+                                # List out the results and check each naming context for failures
+
+                                It "$($entry.'Naming Context') - should have zero replication failures" {
+                                    $entry.'Number of failures' | Should Be 0
+                                }
                             }
                         }
                     }
@@ -84,4 +96,5 @@ Describe -Name 'Domain Controller Infrastructure Test' -Tags 'Infrastructure,Dom
             }
         }
     }
+
 }
